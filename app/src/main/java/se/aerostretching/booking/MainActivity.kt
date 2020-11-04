864575524a2258d1b7555bac994b202ab05c6226
Anastasia <anastasia.dobrova@mail.ru>
2020-11-04 22:09:58 +0100

    added all activities

--- app/src/main/java/se/aerostretching/booking/MainActivity.kt
+++ app/src/main/java/se/aerostretching/booking/MainActivity.kt
@@ -1,14 +1,14 @@
 package se.aerostretching.booking
-
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-
+import com.google.firebase.firestore.FirebaseFirestore
 class MainActivity : AppCompatActivity() {
-
+    lateinit var db: FirebaseFirestore
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
+        db = FirebaseFirestore.getInstance()
+        db.collection("users").add(Item("Anna", "Olsson"))
     }
-
+    data class Item(var firstname: String? = null, var lastname: String? = null)
 }
