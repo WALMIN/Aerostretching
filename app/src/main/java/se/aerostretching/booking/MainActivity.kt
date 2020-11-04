a42621429e8e91e0b7187e812e55812877ced4e8
briggitteMartinez <briggitte90@hotmail.com>
2020-11-04 17:37:07 +0100

    Connected app to firebase and added user to firestore

--- app/src/main/java/se/aerostretching/booking/MainActivity.kt
+++ app/src/main/java/se/aerostretching/booking/MainActivity.kt
@@ -2,13 +2,21 @@ package se.aerostretching.booking
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.firebase.firestore.FirebaseFirestore
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var db: FirebaseFirestore
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        db = FirebaseFirestore.getInstance()
+        db.collection("users").add(Item("Briggan", "Martinez"))
+
     }
+
+    data class Item(var firstname: String? = null, var lastname: String? = null)
 
 }
