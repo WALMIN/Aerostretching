a0b90cfb9be03fb8a6c7878b28b2fa8549f9376d
Anastasia <anastasia.dobrova@mail.ru>
2020-11-04 22:11:10 +0100

    added all activities

---
+++ app/src/main/java/se/aerostretching/booking/MessagesActivity.kt
@@ -0,0 +1,11 @@
+package se.aerostretching.booking
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MessagesActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_messages)
+    }
+}
---
+++ app/src/main/java/se/aerostretching/booking/RegisterActivity.kt
@@ -0,0 +1,11 @@
+package se.aerostretching.booking
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class RegisterActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_register)
+    }
+}
