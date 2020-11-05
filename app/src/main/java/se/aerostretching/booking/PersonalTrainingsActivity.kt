d05253b7defe998f234efa9a2de900598d5e8358
Victor Bergene <v@walmin.com>
2020-11-05 13:35:57 +0100

    Setup basic functions for personal trainings

--- app/src/main/java/se/aerostretching/booking/PersonalTrainingsActivity.kt
+++ app/src/main/java/se/aerostretching/booking/PersonalTrainingsActivity.kt
@@ -2,10 +2,40 @@ package se.aerostretching.booking
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.EditText
+import android.widget.Toast
 
 class PersonalTrainingsActivity : AppCompatActivity() {
+
+    lateinit var editTextApply: EditText
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_personal_trainings)
+        initialize()
+
     }
+
+    fun initialize(){
+        editTextApply = findViewById(R.id.editTextApply)
+
+    }
+
+    fun applyPersonalTraining(view: View){
+        if(editTextApply.text.toString().trim().isNotEmpty()){
+            sendPersonalTraining()
+
+        }else{
+            Toast.makeText(this, "Du måste skriva något först.", Toast.LENGTH_LONG).show()
+
+        }
+
+    }
+
+    fun sendPersonalTraining(){
+        Toast.makeText(this, "(send request)", Toast.LENGTH_LONG).show()
+
+    }
+
 }
