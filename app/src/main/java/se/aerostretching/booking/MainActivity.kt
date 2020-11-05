1b0c462d48f81f3afbc9d62a9309a5db710b5d8c
briggitteMartinez <briggitte90@hotmail.com>
2020-11-05 07:20:18 +0100

    //

--- app/src/main/java/se/aerostretching/booking/MainActivity.kt
+++ app/src/main/java/se/aerostretching/booking/MainActivity.kt
@@ -13,7 +13,7 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         db = FirebaseFirestore.getInstance()
-        db.collection("users").add(Item("Briggan", "Martinez"))
+       // db.collection("users").add(Item("Briggan", "Martinez"))
 
     }
 
