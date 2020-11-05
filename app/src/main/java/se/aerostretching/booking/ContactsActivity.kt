7276e3a354e0a775923a72a830e47df71724fa69
Victor Bergene <v@walmin.com>
2020-11-05 10:50:30 +0100

    Added onClickListener

--- app/src/main/java/se/aerostretching/booking/ContactsActivity.kt
+++ app/src/main/java/se/aerostretching/booking/ContactsActivity.kt
@@ -2,10 +2,41 @@ package se.aerostretching.booking
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 
 class ContactsActivity : AppCompatActivity() {
+
+    lateinit var textViewContactsPhone: TextView
+    lateinit var textViewContactsEmail: TextView
+    lateinit var textViewContactsPlace: TextView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_contacts)
+
+        initialize()
+
     }
+
+    fun initialize(){
+        textViewContactsPhone = findViewById(R.id.textViewContactsPhone)
+            textViewContactsPhone.setOnClickListener{
+
+
+            }
+
+        textViewContactsEmail = findViewById(R.id.textViewContactsEmail)
+            textViewContactsEmail.setOnClickListener{
+
+
+            }
+
+        textViewContactsPlace = findViewById(R.id.textViewContactsPlace)
+            textViewContactsPlace.setOnClickListener{
+
+
+            }
+
+    }
+
 }
