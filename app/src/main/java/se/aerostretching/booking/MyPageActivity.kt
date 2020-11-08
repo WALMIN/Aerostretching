6de125d4a466db20cb3731f01b9ae29b5e3e3577
ud <maab970514@student.jenseneducation.se>
2020-11-08 17:39:25 +0100

    Created constraints and set an onclickListener and an intent on profile button

--- app/src/main/java/se/aerostretching/booking/MyPageActivity.kt
+++ app/src/main/java/se/aerostretching/booking/MyPageActivity.kt
@@ -1,11 +1,24 @@
 package se.aerostretching.booking
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageButton
 
 class MyPageActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_my_page)
+
+
+                val imageButton1 = findViewById<ImageButton>(R.id.btn_Profile)
+
+           imageButton1.setOnClickListener{
+           val intent = Intent(this,ProfileActivity::class.java)
+           startActivity(intent)
+
+
+       }
+
     }
 }
