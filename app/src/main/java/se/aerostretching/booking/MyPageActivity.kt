01d7a9ac88f03c4e8cd40c28046abaa2874aa4c0
ud <maab970514@student.jenseneducation.se>
2020-11-09 15:32:27 +0100

    added custombar to myPage

--- app/src/main/java/se/aerostretching/booking/MyPageActivity.kt
+++ app/src/main/java/se/aerostretching/booking/MyPageActivity.kt
@@ -3,12 +3,17 @@ package se.aerostretching.booking
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.widget.ImageButton
+import android.widget.TextView
+import androidx.appcompat.app.ActionBar
 
 class MyPageActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_my_page)
+
+        customActionBar()
 
 
         val imageButton1 = findViewById<ImageButton>(R.id.btn_Profile)
@@ -35,6 +40,38 @@ class MyPageActivity : AppCompatActivity() {
             val intent4 = Intent(this, MessagesActivity::class.java)
             startActivity(intent4)
         }
+
+    }
+
+    fun customActionBar() {
+        supportActionBar!!.displayOptions = ActionBar.DISPLAY_SHOW_CUSTOM
+        supportActionBar!!.setDisplayShowCustomEnabled(true)
+        supportActionBar!!.setCustomView(R.layout.action_bar)
+        val view = supportActionBar!!.customView
+
+        // Title
+        val titleView = view.findViewById<View>(R.id.title) as TextView
+        titleView.text = "My page"
+
+        // Start button
+        val startBtn = view.findViewById<View>(R.id.startBtn) as ImageButton
+        startBtn.visibility = View.VISIBLE
+        startBtn.setImageResource(R.drawable.menu)
+        startBtn.setOnClickListener {
+
+
+        }
+
+        // End button
+        val endBtn = view.findViewById<View>(R.id.endBtn) as ImageButton
+        endBtn.visibility = View.VISIBLE
+        endBtn.setImageResource(R.drawable.logout)
+        endBtn.setOnClickListener {
+
+
+        }
+
+
     }
 }
 
