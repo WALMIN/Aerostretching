254550fcad981902277c68ccb835ff5ec78b2f9f
ud <maab970514@student.jenseneducation.se>
2020-11-09 14:56:50 +0100

    added intents to all the image buttons

--- app/src/main/java/se/aerostretching/booking/MyPageActivity.kt
+++ app/src/main/java/se/aerostretching/booking/MyPageActivity.kt
@@ -11,14 +11,32 @@ class MyPageActivity : AppCompatActivity() {
         setContentView(R.layout.activity_my_page)
 
 
-                val imageButton1 = findViewById<ImageButton>(R.id.btn_Profile)
+        val imageButton1 = findViewById<ImageButton>(R.id.btn_Profile)
+        val imageButton2 = findViewById<ImageButton>(R.id.imageButtonTrainings)
+        val imageButton3 = findViewById<ImageButton>(R.id.imageButtonHistory)
+        val imageButton4 = findViewById<ImageButton>(R.id.imageButtonMessages)
 
-           imageButton1.setOnClickListener{
-           val intent = Intent(this,ProfileActivity::class.java)
-           startActivity(intent)
+        imageButton1.setOnClickListener {
+            val intent = Intent(this, ProfileActivity::class.java)
+            startActivity(intent)
+        }
 
+        imageButton2.setOnClickListener {
+            val intent2 = Intent(this, PersonalTrainingsActivity::class.java)
+            startActivity(intent2)
+        }
 
-       }
+        imageButton3.setOnClickListener {
+            val intent3 = Intent(this, HistoryActivity::class.java)
+            startActivity(intent3)
+        }
 
+        imageButton4.setOnClickListener {
+            val intent4 = Intent(this, MessagesActivity::class.java)
+            startActivity(intent4)
+        }
     }
 }
+
+
+
