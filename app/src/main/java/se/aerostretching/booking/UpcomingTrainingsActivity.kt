a91b0f4929676e390c70d5b5c0b97af23ee29060
Victor Bergene <v@walmin.com>
2020-11-09 15:49:30 +0100

    Added a custom action bar to 4 new activities

--- app/src/main/java/se/aerostretching/booking/UpcomingTrainingsActivity.kt
+++ app/src/main/java/se/aerostretching/booking/UpcomingTrainingsActivity.kt
@@ -1,11 +1,40 @@
 package se.aerostretching.booking
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.ImageButton
+import android.widget.TextView
+import androidx.appcompat.app.ActionBar
 
 class UpcomingTrainingsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_upcoming_trainings)
+        customActionBar()
+
     }
+
+    fun customActionBar(){
+        supportActionBar!!.displayOptions = ActionBar.DISPLAY_SHOW_CUSTOM
+        supportActionBar!!.setDisplayShowCustomEnabled(true)
+        supportActionBar!!.setCustomView(R.layout.action_bar)
+        val view = supportActionBar!!.customView
+
+        // Title
+        val titleView = view.findViewById<View>(R.id.title) as TextView
+        titleView.text = "Upcoming trainings"
+
+        // Start button
+        val startBtn = view.findViewById<View>(R.id.startBtn) as ImageButton
+        startBtn.visibility = View.VISIBLE
+        startBtn.setImageResource(R.drawable.back)
+        startBtn.setOnClickListener {
+            startActivity(Intent(this, MyPageActivity::class.java))
+
+        }
+
+    }
+
 }
