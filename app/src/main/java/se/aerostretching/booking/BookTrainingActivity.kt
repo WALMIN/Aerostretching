4aa7ed57b66cd6f63a322619566d40c9bbee3e30
Anastasia <anastasia.dobrova@mail.ru>
2020-11-10 22:49:32 +0100

    Book training Activity added

--- app/src/main/java/se/aerostretching/booking/BookTrainingActivity.kt
+++ app/src/main/java/se/aerostretching/booking/BookTrainingActivity.kt
@@ -4,18 +4,43 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.Button
 import android.widget.ImageButton
 import android.widget.TextView
 import androidx.appcompat.app.ActionBar
 
 class BookTrainingActivity : AppCompatActivity() {
 
+
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
+
+
+
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_book_training)
         customActionBar()
-
+        val buttonBook = findViewById<Button>(R.id.buttonBook)
+        buttonBook.setOnClickListener {
+            goToUpcomingTrainingsActivity()
+        }
+        val btn_trainer = findViewById<ImageButton>(R.id.btn_trainer)
+        btn_trainer.setOnClickListener {
+            goToTrainerActivity()
+        }
     }
+
+    fun goToUpcomingTrainingsActivity() {
+        val intent = Intent(this, UpcomingTrainingsActivity::class.java)
+        startActivity(intent)
+    }
+
+    fun goToTrainerActivity() {
+        val intent = Intent(this, TrainerActivity::class.java)
+        startActivity(intent)
+    }
+
 
     fun customActionBar(){
         supportActionBar!!.displayOptions = ActionBar.DISPLAY_SHOW_CUSTOM
@@ -42,8 +67,9 @@ class BookTrainingActivity : AppCompatActivity() {
         endBtn.setImageResource(R.drawable.add_event)
         endBtn.setOnClickListener {
 
-
         }
+
+
 
     }
 
