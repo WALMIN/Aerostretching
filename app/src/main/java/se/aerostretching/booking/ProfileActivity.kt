ac7e118dd01b4332013ca039f7417ec770e66704
ud <maab970514@student.jenseneducation.se>
2020-11-12 15:05:59 +0100

    Connected profilePage to Firebase

--- app/src/main/java/se/aerostretching/booking/ProfileActivity.kt
+++ app/src/main/java/se/aerostretching/booking/ProfileActivity.kt
@@ -1,19 +1,71 @@
 package se.aerostretching.booking
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.View
+import android.widget.EditText
 import android.widget.ImageButton
 import android.widget.TextView
 import androidx.appcompat.app.ActionBar
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.FragmentActivity
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.FirebaseFirestore
+
 
 class ProfileActivity : AppCompatActivity() {
+
+
+    lateinit var editTextName: EditText
+    lateinit var editTextBirth: EditText
+    lateinit var editTextEmail: EditText
+    lateinit var editTextPhone: EditText
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_profile)
         customActionBar()
+
+
+         editTextName = findViewById<EditText>(R.id.userName)
+         editTextBirth = findViewById<EditText>(R.id.dateOfBirth)
+         editTextEmail = findViewById<EditText>(R.id.mailAccount)
+         editTextPhone = findViewById<EditText>(R.id.phoneNumber)
+
+        getUserInfo()
+
+
+
+    }
+
+    fun getUserInfo(){
+
+        FirebaseFirestore.getInstance().collection("users").document(FirebaseAuth.getInstance().currentUser?.uid.toString()).collection("info")
+                .get()
+                .addOnCompleteListener { task ->
+                    if (task.isSuccessful) {
+                        for (document in task.result!!) {
+                            Log.d("!!!", document.id + " => " + document.data)
+
+                            editTextName.setText (document.getString("name").toString())
+                            editTextBirth.setText (document.getString("birth").toString())
+                            editTextEmail.setText (document.getString("email").toString())
+                            editTextPhone.setText (document.getString("phone").toString())
+
+                        }
+                    } else {
+                        Log.w("!!!", "Error getting documents.", task.exception)
+                    }
+                }
+
+
+
+
+
+
+
 
     }
 
@@ -44,6 +96,7 @@ class ProfileActivity : AppCompatActivity() {
             startActivity(Intent(this, ContactsActivity::class.java))
 
         }
+
 
     }
 
