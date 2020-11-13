836ca61ce6c82c88fdad643f817f3580c6449f5c
Victor Bergene <v@walmin.com>
2020-11-13 21:33:52 +0100

    Shows only 3 trainings on StartActivity & all trainings on ScheduleBookingActivity

--- app/src/main/java/se/aerostretching/booking/GetData.kt
+++ app/src/main/java/se/aerostretching/booking/GetData.kt
@@ -10,6 +10,7 @@ import kotlin.collections.ArrayList
 object GetData {
 
     lateinit var trainingListAdapter: TrainingListAdapter
+    val trainingListStart = ArrayList<TrainingItem>()
     val trainingList = ArrayList<TrainingItem>()
 
     var name = ""
@@ -17,53 +18,66 @@ object GetData {
     var email = ""
     var phone = ""
 
-    fun trainings(){
+    fun trainings() {
         FirebaseFirestore.getInstance().collection("trainings").orderBy("date")
-            .whereGreaterThanOrEqualTo("date", SimpleDateFormat("MMddyyyy", Locale.getDefault()).format(Calendar.getInstance().time))
-            .limit(5)
-            .addSnapshotListener { snapshot, e ->
-                Log.d("!!!", "READ")
+                .whereGreaterThanOrEqualTo("date", SimpleDateFormat("MMddyyyy", Locale.getDefault()).format(Calendar.getInstance().time))
+                .addSnapshotListener { snapshot, e ->
+                    Log.d("!!!", "READ")
 
-                trainingList.clear()
+                    trainingList.clear()
+                    trainingListStart.clear()
 
-                for (document in snapshot!!) {
-                    //Log.d("!!!", document.id + " => " + document.data)
+                    // All trainings
+                    for (document in snapshot!!) {
+                        trainingList.add(TrainingItem(
+                                document.getString("date").toString(),
+                                document.getString("time").toString(),
+                                document.getString("length").toString(),
+                                document.getString("title").toString(),
+                                document.getString("place").toString(),
+                                document.getString("trainer").toString(),
+                                document.getString("spots").toString()
+                        ))
 
-                    trainingList.add(TrainingItem(
-                            document.getString("date").toString(),
-                            document.getString("time").toString(),
-                            document.getString("length").toString(),
-                            document.getString("title").toString(),
-                            document.getString("place").toString(),
-                            document.getString("trainer").toString(),
-                            document.getString("spots").toString()
-                    ))
+                    }
+
+                    // Start trainings
+                    for (i in 0..2) {
+                        trainingListStart.add(TrainingItem(
+                                snapshot.documents[i].getString("date").toString(),
+                                snapshot.documents[i].getString("time").toString(),
+                                snapshot.documents[i].getString("length").toString(),
+                                snapshot.documents[i].getString("title").toString(),
+                                snapshot.documents[i].getString("place").toString(),
+                                snapshot.documents[i].getString("trainer").toString(),
+                                snapshot.documents[i].getString("spots").toString()
+                        ))
+
+                    }
+                    trainingListAdapter.notifyDataSetChanged()
 
                 }
-                trainingListAdapter.notifyDataSetChanged()
-
-            }
 
     }
 
-    fun profile(){
+    fun profile() {
         FirebaseFirestore.getInstance().collection("users").document(FirebaseAuth.getInstance().currentUser?.uid.toString()).collection("info")
-            .get()
-            .addOnCompleteListener { task ->
-                if (task.isSuccessful) {
-                    for (document in task.result!!) {
-                        Log.d("!!!", document.id + " => " + document.data)
+                .get()
+                .addOnCompleteListener { task ->
+                    if (task.isSuccessful) {
+                        for (document in task.result!!) {
+                            Log.d("!!!", document.id + " => " + document.data)
 
-                        name = document.getString("name").toString()
-                        birth = document.getString("birth").toString()
-                        email = document.getString("email").toString()
-                        phone = document.getString("phone").toString()
+                            name = document.getString("name").toString()
+                            birth = document.getString("birth").toString()
+                            email = document.getString("email").toString()
+                            phone = document.getString("phone").toString()
 
+                        }
+                    } else {
+                        Log.w("!!!", "Error getting documents.", task.exception)
                     }
-                } else {
-                    Log.w("!!!", "Error getting documents.", task.exception)
                 }
-            }
 
     }
 
