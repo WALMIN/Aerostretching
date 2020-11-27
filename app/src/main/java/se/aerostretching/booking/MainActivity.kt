8c65abdd90b3b395dbd60df183b5ac68ae9bf537
Victor Bergene <v@walmin.com>
2020-11-27 14:50:27 +0100

    Added forgot password button with dialog

--- app/src/main/java/se/aerostretching/booking/MainActivity.kt
+++ app/src/main/java/se/aerostretching/booking/MainActivity.kt
@@ -8,10 +8,13 @@ import android.view.View
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
+import android.widget.Toast
 import androidx.appcompat.app.ActionBar
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import com.bumptech.glide.Glide
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.FirebaseFirestore
 import com.google.firebase.storage.FirebaseStorage
 import se.aerostretching.booking.LocaleHelper.onAttach
 
@@ -84,6 +87,37 @@ class MainActivity : AppCompatActivity() {
                 }
 
             }
+    }
+
+    fun forgotPasswordBtn(view: View){
+        val dialogLayout = layoutInflater.inflate(R.layout.dialog_forgot_password, null)
+        val editText = dialogLayout.findViewById<EditText>(R.id.editTextMsg)
+
+        AlertDialog.Builder(this)
+            .setTitle(R.string.forgotPassword)
+            .setView(dialogLayout)
+            .setPositiveButton(getString(R.string.sendTraining)) { dialog, i ->
+                if(editText.text.toString().isNotEmpty()) {
+                    auth.sendPasswordResetEmail(editText.text.toString())
+                        .addOnCompleteListener { task ->
+                            if (task.isSuccessful) {
+                                Toast.makeText(this, getString(R.string.resetPasswordSent), Toast.LENGTH_SHORT).show()
+                                Log.d("!!!", "Email sent")
+                                dialog.dismiss()
+                            }else{
+                                Toast.makeText(this, getString(R.string.errorSending), Toast.LENGTH_SHORT).show()
+
+                            }
+                        }
+
+                }else{
+                    Toast.makeText(this, getString(R.string.noTrainingText), Toast.LENGTH_LONG).show()
+
+                }
+
+            }
+            .show()
+
     }
 
     fun customActionBar() {
